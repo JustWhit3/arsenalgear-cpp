cb918e2cde382c1825fb85d065ec2010773903d0
Carl Timmer <timmer@jlab.org>
2022-07-05 22:51:49 +0000

    Update docs

--- docs/search/all_3.js
+++ docs/search/all_3.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['empty_5fspace_4',['empty_space',['../namespaceagr.html#af68907b37ef6e4abe00f6cf74778cde0',1,'agr']]],
-  ['except_5ferror_5ffunc_5',['except_error_func',['../namespaceagr.html#a0041d14a0e49826455e3fa1ad9807bd1',1,'agr']]]
+  ['except_5ferror_5ffunc_5',['except_error_func',['../namespaceagr.html#a4cb5be05e5a1b3bfee208fc9aebb7fa4',1,'agr']]]
 ];
--- docs/search/functions_1.js
+++ docs/search/functions_1.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['except_5ferror_5ffunc_43',['except_error_func',['../namespaceagr.html#a0041d14a0e49826455e3fa1ad9807bd1',1,'agr']]]
+  ['except_5ferror_5ffunc_43',['except_error_func',['../namespaceagr.html#a4cb5be05e5a1b3bfee208fc9aebb7fa4',1,'agr']]]
 ];
