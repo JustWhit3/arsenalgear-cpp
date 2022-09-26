c9c58a74f2fa6e51cf5b7e99779bbc3fa1cb59ae
Gianluca Bianco <gianluca.bianco3@studio.unibo.it>
2022-09-26 17:50:05 +0200

    Added select_cout and select_wcout structs.

--- include/stream.hpp
+++ include/stream.hpp
@@ -19,15 +19,18 @@
 //     Headers
 //====================================================
 
-//STD headers
+// STD headers
+#include <iostream>
 #include <string>
 #include <streambuf>
 
 namespace agr
  {
   //====================================================
-  //     savebuf class
+  //     Classes
   //====================================================
+
+  // savebuf
   /**
    * @brief Class used to store the output sent to an output stream.
    * 
@@ -56,6 +59,42 @@ namespace agr
     std::streambuf* sbuf;
     std::string     save;
    };
+
+  //====================================================
+  //     Structs
+  //====================================================
+  
+  // select_cout
+  /**
+   * @brief Struct used to define a way to template the choice of the "std::cout" object in order to be "std::cout" for "char" type or "std::wcout" for "wchar_t" type.
+   * 
+   * @tparam T The template type of the "std::cout" object (char, wchar_t, ...).
+   */  
+  template <class T> 
+  struct select_cout
+   {
+    static std::basic_ostream<T> &cout;
+   };
+
+  // select_cout specializations
+  template <> inline std::ostream &select_cout <char>::cout = std::cout;
+  template <> inline std::wostream &select_cout <wchar_t>::cout = std::wcout;
+
+  // select_cin
+  /**
+   * @brief Struct used to define a way to template the choice of the "std::cin" object in order to be "std::cin" for "char" type or "std::wcin" for "wchar_t" type.
+   * 
+   * @tparam T The template type of the "std::cin" object (char, wchar_t, ...).
+   */  
+  template <class T> 
+  struct select_cin
+   {
+    static std::basic_istream<T> &cin;
+   };
+
+  // select_cin specializations
+  template <> inline std::istream &select_cin <char>::cin = std::cin;
+  template <> inline std::wistream &select_cin <wchar_t>::cin = std::wcin;
  }
 
 #endif
--- test/tests_stream.cpp
+++ test/tests_stream.cpp
@@ -29,3 +29,21 @@ TEST_CASE( "Testing the savebuf class." )
   std::cout.rdbuf( coutbuf );
   CHECK_EQ( sbuf.str(), "Test" );
  }
+
+//============================================
+//     select_cout
+//============================================
+TEST_CASE( "Testing select_cout" )
+ {
+  CHECK_EQ( &agr::select_cout<char>::cout, &std::cout );
+  CHECK_EQ( &agr::select_cout<wchar_t>::cout, &std::wcout );
+ }
+
+//============================================
+//     select_cin
+//============================================
+TEST_CASE( "Testing select_cin" )
+ {
+  CHECK_EQ( &agr::select_cin<char>::cin, &std::cin );
+  CHECK_EQ( &agr::select_cin<wchar_t>::cin, &std::wcin );
+ }
