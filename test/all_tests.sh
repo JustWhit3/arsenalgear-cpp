80f9a1589fd428fa8418b373bc39e43023e23912
Gianluca Bianco <48323961+JustWhit3@users.noreply.github.com>
2022-09-27 15:42:42 +0200

    Update all_tests.sh

--- test/all_tests.sh
+++ test/all_tests.sh
@@ -73,4 +73,4 @@ echo "======================================================"
 echo "     CPPCHECK TESTS"
 echo "======================================================"
 echo ""
-cppcheck include/* src*
+cppcheck include/* src/*
