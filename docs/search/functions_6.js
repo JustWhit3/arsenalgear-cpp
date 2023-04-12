4eec3dac08e5856572e6690bdbee77347790bb78
Carl Timmer <timmer@jlab.org>
2023-04-12 13:49:37 +0000

    Update docs

--- docs/search/functions_6.js
+++ docs/search/functions_6.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['roundoff_81',['roundoff',['../namespaceagr.html#a29b5429193e1316a42ca73e8123a8bd5',1,'agr']]]
+  ['roundoff_81',['roundoff',['../namespaceagr.html#ab632f9e8e52e8cad544fb1c01fdddc16',1,'agr']]]
 ];
