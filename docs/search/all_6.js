8172fa8d3d30a50a5e503ef185e36df7cb04254d
Carl Timmer <timmer@jlab.org>
2023-04-12 09:58:19 +0000

    Update docs

--- docs/search/all_6.js
+++ docs/search/all_6.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['math_2ehpp_29',['math.hpp',['../math_8hpp.html',1,'']]],
-  ['multi_30',['multi',['../namespaceagr.html#a26849e3e5a1662f89d1267093f8eb158',1,'agr']]]
+  ['multi_30',['multi',['../namespaceagr.html#a562d67b1ade6b34802403c2736125959',1,'agr']]]
 ];
--- docs/search/functions_5.js
+++ docs/search/functions_5.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['one_79',['one',['../namespaceagr.html#a79c1d16a030bfa822fcf06fcfd56db5a',1,'agr']]],
-  ['operator_2a_80',['operator*',['../operators_8hpp.html#aa21cb9291ed5e17a8b54f4538e72aa33',1,'operator*(const T &amp;generic_string, unsigned int integer):&#160;operators.hpp'],['../operators_8hpp.html#aa7679c32f6fc69c7c2c031e0a8fec67d',1,'operator*(unsigned int integer, const T &amp;generic_string):&#160;operators.hpp']]]
+  ['operator_2a_80',['operator*',['../operators_8hpp.html#aae54e930f345ceefa80a59b5bcaa4fee',1,'operator*(const T &amp;generic_string, uint32_t integer):&#160;operators.hpp'],['../operators_8hpp.html#a7c25182a85751d8b2158409e827c03c9',1,'operator*(uint32_t integer, const T &amp;generic_string):&#160;operators.hpp']]]
 ];
