6a4067eaf97d97e109ed464b05425dc13bd23b31
Carl Timmer <timmer@jlab.org>
2023-04-12 13:29:25 +0000

    Update docs

--- docs/search/all_5.js
+++ docs/search/all_5.js
@@ -7,5 +7,5 @@ var searchData=
   ['is_5fstreamable_25',['is_streamable',['../structagr_1_1is__streamable.html',1,'agr']]],
   ['is_5fstreamable_3c_20s_2c_20t_2c_20std_3a_3avoid_5ft_3c_20decltype_28std_3a_3adeclval_3c_20s_20_26_20_3e_28_29_3c_3c_20std_3a_3adeclval_3c_20t_20_3e_28_29_29_3e_20_3e_26',['is_streamable&lt; S, T, std::void_t&lt; decltype(std::declval&lt; S &amp; &gt;()&lt;&lt; std::declval&lt; T &gt;())&gt; &gt;',['../structagr_1_1is__streamable_3_01S_00_01T_00_01std_1_1void__t_3_01decltype_07std_1_1declval_3_01S07be0fae6b13f93697c0fcdbe88a7bf8.html',1,'agr']]],
   ['isfloatingpoint_27',['isFloatingPoint',['../namespaceagr.html#a30f754b298e55c68819829df78d936cb',1,'agr']]],
-  ['isinbounds_28',['IsInBounds',['../namespaceagr.html#ae892e70efaebcc7c3886969a03b2e264',1,'agr']]]
+  ['isinbounds_28',['IsInBounds',['../namespaceagr.html#a631f02dbbfad7ff146ef86e89d2ddb4c',1,'agr']]]
 ];
